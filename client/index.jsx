fb3610d2c229665330f74b82a18831caeae78560
kkanchamreddy <kiran.kanchamreddy@gmail.com>
2016-03-25 06:47:42 -0700

    Hook up with Redux store

--- client/index.jsx
+++ client/index.jsx
@@ -4,9 +4,28 @@ import { Router }  from 'react-router';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 import routes      from 'routes';
 
+import { createStore, combineReducers } from 'redux';
+import { Provider }                     from 'react-redux';
+import * as reducers                    from 'reducers';
+import { fromJS }                       from 'immutable';
+
 import './style.css'
 const history = createBrowserHistory();
+let initialState = window.__INITIAL_STATE__;
+// Transform into Immutable.js collections,
+// but leave top level keys untouched for Redux
+Object
+  .keys(initialState)
+  .forEach(key => {
+    initialState[key] = fromJS(initialState[key]);
+   });
+
+const reducer = combineReducers(reducers);
+const store   = createStore(reducer, initialState);
+
 render(
-  <Router children={routes} history={history} />,
+  <Provider store={store}>
+      <Router children={routes} history={history} />
+  </Provider>,
   document.getElementById('react-view')
 );
